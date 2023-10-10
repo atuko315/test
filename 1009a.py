cfd985978567edd340546e5a2da8424263b3abf5
atuko315 <uguisu315@keio.jp>
2023-10-10 19:32:47 +0900

    update

--- 1009a.py
+++ 1009a.py
@@ -17,7 +17,7 @@ import random
 from time import sleep
 from collections import defaultdict
 import collections
-#パターンの出現場所
+#パターンの出現場所 v2, h2
 #factor = 31
 
 sample_s_path = '/home/student/PARL/benchmark/torch/AlphaZero/best_200.pth.tar'
@@ -38,31 +38,42 @@ paths4 = sorted(Path('./label/trivial/trivial/short').glob('*.board'))
 
 paths = [paths1, paths2, paths3, paths4]
 height, width = game.getBoardSize()
+datas = []
 for i in range(len(paths)):
-    print(f"dataset: {i+1}")
+    #print(f"dataset: {i+1}")
     record = defaultdict(lambda: 0)
     dataset = DatasetManager(game, paths[i])
     dataset.make_board_set()
-    pattern_paths = dataset.make_pattern_path_set(dataset.pattern_set[0])
+    pattern_paths = dataset.make_pattern_path_set(dataset.pattern_set[1])
     pdataset = DatasetManager(game, pattern_paths)
     for p in pdataset.path_set:
         content = load_data(p)
         imp, board, branch, fpath, importance = content
-        contain_indices, pure_indices = pdataset.match_pattern(board, dataset.pattern_set[0])
+        contain_indices, pure_indices = pdataset.match_pattern(board, dataset.pattern_set[1])
         for c in contain_indices:
             h = int(c/width)
             w = c % width
+            '''
             if h > 0:
                 record[c-width] += 1
+            '''
+            
             record[c] += 1
     
     visual = [0 if i not in collections.Counter(record).keys() else collections.Counter(record)[i]
                 for i in range(height * width)]
     visual = np.array(visual).reshape(height, width)
-    print(np.sum(visual, axis=0)) #列ごと
-    print(np.sum(visual, axis=1)) #行ごと
-    print(visual)
+    #print(np.sum(visual, axis=0)) #列ごと
+    data = np.sum(visual, axis=0)
+    data = np.array(data, dtype=np.float64)
+    data /= np.sum(data)
+    datas.append(data)
+    #print(np.sum(visual, axis=1)) #行ごと
+    #print(visual)
 
+#print(datas)
+for i in range(len(datas[0])):
+    print(f"{i} {datas[0][i]} {datas[1][i]} {datas[2][i]} {datas[3][i]}")
 
         
 
